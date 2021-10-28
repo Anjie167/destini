d32dd7d284e2cd0cbf8c955b01ca880017fd15d0
anjie167 <anjie167@gmail.com>
2021-10-28 08:16:49 +0100

    Initial commit

---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  String storyTitle;
+  String choice1; //store the text for choice 1,
+  String choice2; //to store the text for choice 2.
+
+  Story({String storyTitle, String choice1, String choice2}){
+    this.storyTitle=storyTitle;
+    this.choice1=choice1;
+    this.choice2=choice2;
+  }
+}
